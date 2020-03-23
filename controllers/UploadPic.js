e9a42b618d3e53b40aeb20e6b6452d1602ec1f15
alex_zhooson <861077046@qq.com>
2020-03-23 20:08:20 +0800

    fix 删除多余文件代码

--- controllers/UploadPic.js
+++ controllers/UploadPic.js
@@ -29,34 +29,49 @@ class UploadPic {
             // 上传单个文件
             const file = ctx.request.files.file; // 获取上传文件
             const { bucket = 'test' } = ctx.request.body;
-            console.info('bucket', bucket)
+            // console.info('bucket', bucket)
 
             // 生成文件夹
             let dir = path.join(__dirname, `../public/images/${bucket}`);
-            checkDirExist(dir)
-            console.info('dir', dir, file)
+            checkDirExist(dir);
+            // console.info('dir', dir, file)
 
             // 生成图片文件名字
             let newName = getUploadFileExt(file.name);
-            console.info('newName', newName);
+            // console.info('newName', newName);
 
             // 文件目录
-            let filePath = `${dir}/${newName}`
-            console.info('filePath', filePath)
+            let filePath = `${dir}/${newName}`;
+            // console.info('filePath', filePath)
 
             // 创建可读流
             const reader = fs.createReadStream(file.path);
 
             // 创建可写流
             const upStream = fs.createWriteStream(filePath);
-            console.info('upStream', upStream)
+            // console.info('upStream', upStream)
 
             // 可读流通过管道写入可写流
             reader.pipe(upStream);
 
-            let urlstr = `${ctx.origin}/images/${bucket}/${newName}`
+            // ------ starting ------
+            // 删除文件(可以注释这段代码，不明白为什么产生一些重复的文件)
+            let delpath = path.join(__dirname, `../public/images`);
+            let files = fs.readdirSync(delpath);
+            // console.info(234, delpath, files)
+            files.forEach(item => {
+                // 判断 是不是文件夹 【true: 文件夹】
+                if (!fs.lstatSync(`${delpath}/${item}`).isDirectory()) {
+                    if (item.indexOf('upload_') > -1) {
+                        fs.unlink(`${delpath}/${item}`)
+                    }
+                }
+            });
+            // ------ ending ------
 
+            let urlstr = `${ctx.origin}/images/${bucket}/${newName}`
             CallbackModel(ctx, 200, '上传成功！', { url: urlstr })
+
         } catch (err) {
             console.info(33, err)
             CallbackModel(ctx, 500, '上传失败！', JSON.stringify(err))
