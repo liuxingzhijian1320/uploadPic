07152d9bf95c918c18e0cfbc16289fd99e311129
alex_zhooson <861077046@qq.com>
2020-03-23 20:11:08 +0800

    fix cors

--- uploadpic-app-3092.js
+++ uploadpic-app-3092.js
@@ -16,24 +16,24 @@ const { port } = require('./config')
 const { judgeHttpUrl } = require('./utils')
 
 // 允许所有的跨域
-app.use(cors());
+// app.use(cors());
 // https://www.npmjs.com/package/koa2-cors
-// // 设置跨域
-// app.use(cors({
-//     origin: function(ctx) {
-//         // console.log(111, ctx, ctx.url)
-//         // 设置白名单
-//         if (judgeHttpUrl(ctx.header.host)) {
-//             return '*';
-//         }
-//         // return ['http://localhost:3092', 'http://www.zhooson.cn']; // 指定域名访问
-//     },
-//     exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'], // 获取额外的header信息
-//     maxAge: 5, //  该字段可选，用来指定本次预检请求的有效期，单位为秒
-//     credentials: true,
-//     allowMethods: ['GET', 'POST', 'DELETE', 'PUT', 'UPDATE', 'OPTIONS'], // 请求允许的方法
-//     allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'x-requested-with'] // 允许的header字段名
-// }))
+// 设置跨域
+app.use(cors({
+    origin: function(ctx) {
+        // console.log(111, ctx, ctx.url)
+        // 设置白名单
+        if (judgeHttpUrl(ctx.header.host)) {
+            return '*';
+        }
+        // return ['http://localhost:3092', 'http://www.zhooson.cn']; // 指定域名访问
+    },
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'], // 获取额外的header信息
+    maxAge: 5, //  该字段可选，用来指定本次预检请求的有效期，单位为秒
+    credentials: true,
+    allowMethods: ['GET', 'POST', 'DELETE', 'PUT', 'UPDATE', 'OPTIONS'], // 请求允许的方法
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'x-requested-with'] // 允许的header字段名
+}))
 
 // error handler
 onerror(app)
